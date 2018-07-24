183611f4e581ebbf17ed9e2948ddc3bfa741ed60
Ruan Azevedo <ruan.azevedo@gmail.com>
2018-07-24 13:47:51 -0300

    Format last commit

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -16,7 +16,7 @@ const CompareList = ({repositories}) => (
                     <li>{repository.stargazers_count} <small>stars</small></li>
                     <li>{repository.forks_count} <small>forks</small></li>
                     <li>{repository.open_issues_count} <small>issues</small></li>
-                    <li>{repository.pushed_at} <small>last commit</small></li>
+                    <li>{repository.lastCommit} <small>last commit</small></li>
                 </ul>
             </Repository>
         ))}
