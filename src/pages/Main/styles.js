f2a5c6f55021b4c8b36868ea58136408fa65b79b
Ruan Azevedo <ruan.azevedo@gmail.com>
2018-07-24 12:33:26 -0300

    first commit

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+`;
+
+export const Form = styled.form`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 55px;
+    background: #FFF;
+    border: 0;
+    font-size: 18px;
+    color: #444;
+    border-radius: 3px;
+    padding: 5px;
+    text-indent: 5px;
+  }
+  
+  button {
+    height: 55px;
+    padding: 0 20px;
+    margin-left: 10px;
+    background: #63fbb0;
+    color: #FFF;
+    border: 0;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 3px;
+    
+    &:hover {
+      background: #52d89f;
+      cursor: pointer;
+    }
+  }
+`;
